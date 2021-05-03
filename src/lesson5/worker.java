4c36643a0383eb13ca5a82cceb17ebdfc1d83d40
Oleg Monastyrev <oleg_monastyrev@mail.ru>
2021-05-03 12:18:14 +0300

    homework5

---
+++ src/lesson5/worker.java
@@ -0,0 +1,23 @@
+package lesson5;
+
+public class worker {
+
+    private String name;
+    private String surname;
+    private String position;
+    private String email;
+    private int number;
+    private int salary;
+    private int age;
+
+    public Worker(String name, String surname, String position, String email, int number, int salary, int age) {
+        this.name=name;
+        this.surname=surname;
+        this.position=position;
+        this.email=email;
+        this.number=number;
+        this.salary=salary;
+        this.age=age;
+    }
+
+}
